0fb1bc1fce2b1c4e9a2cb28e1b98f40ce5695b5d
Bhavreet Singh <bhavreet24singh@gmail.com>
2026-05-30 23:06:12 +0530

    chore: initialize node_modules dependencies for backend project

---
+++ backend/product/src/models/products.model.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+    },
+    price: {
+        amount: {
+            type: Number,
+            required: true
+        },
+        currency: {
+            type: String,
+            enum: ["INR", "USD"],
+            default: "INR",
+        }
+    },
+    stock: {
+        quantity: {
+            type: Number,
+            required: true
+        },
+    },
+    seller: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    images: [{
+        url: String,
+        thumbnail: String,
+        id: String,
+    }],
+
+    varaints: [{
+        type: String,
+        required: true
+    }]
+})
---
+++ backend/product/src/routes/products.routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+//create product
+
+router.post("/",(req, res)=> {
+    
+})
+
+module.exports = router;
